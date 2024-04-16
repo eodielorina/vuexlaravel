2ed95344191a4425a607a2dacc747ca65b51d116
eodielorina <eodielorinah08@gmail.com>
2024-04-16 17:47:16 +0100

    first

---
+++ backend/app/Models/Student.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Student extends Model
+{
+        protected $table= 'students';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    	protected $fillable=['name','phone','email'];
+    use HasFactory;
+    use SoftDeletes;
+            
+}
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import * as bootstrap from "bootstrap";
+import Alert from "bootstrap/js/dist/alert";
+
+import { Tooltip, Toast, Popover } from "bootstrap";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.mount("#app");
